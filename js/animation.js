b0f4dea66da49e7173e5c1124de95a7ecaffdf6d
antn <106150483+AntoineWtz@users.noreply.github.com>
2023-04-02 20:18:35 +0200

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+// animation navbar
+gsap.to(".contact", { duration: 2.5, x: 20, opacity: 1, ease: "elastic", delay: 1 });
+gsap.to("li", { duration: 2.5, x: 10, opacity: 1, ease: "bounce" });
+
+// animation titre + texte home/about
+gsap.to(".sous-titre", { duration: 1.5, x: 10, opacity: 1, ease: "slow" });
+gsap.to(".text-accroche", { duration: 1.5, x: 10, opacity: 1, ease: "slow" });
+gsap.to(".about", { duration: 1.5, x: 10, opacity: 1, ease: "slow" });
---
+++ js/script.js
@@ -0,0 +1,27 @@
+function contactAside() {
+  const contact = document.querySelector('.contact');
+  const aside = document.querySelector('.accueil-aside');
+  let asideContent = false;
+
+  contact.addEventListener('click', toggleAside);
+
+  function toggleAside() {
+    if (!asideContent) {
+      aside.style.backgroundColor = "#827191";
+      aside.innerHTML = `
+          <p class="titre-aside">Bonjour</p>
+          <span>awtz.dev@gmail.com</span>
+          <p>
+            <a href="https://www.linkedin.com/in/antoine-wurtz/" target="_blank">LinkedIn</a>
+            <a href="https://github.com/AntoineWtz" target="_blank">GitHub</a>
+            <a href="./img/CV-Antoine-Wurtz.pdf" target="_blank">CV</a>
+          </p>`;
+      asideContent = true;
+    } else {
+      aside.innerHTML = "";
+      asideContent = false;
+      aside.style.backgroundColor = "#13282B";
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", contactAside);
