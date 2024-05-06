169302ea673385845484680251886b580e5a9eb0
Antoine Wurtz <antoine.wurtz@crowdaa.com>
2024-05-06 14:43:35 +0200

    add projects data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,93 +3,56 @@
 import React from 'react';
 import { useState } from 'react';
 import ProjectCard from './ProjectCard';
+import portfolio from '../img/img-project/portfolio.png';
+import beecloon from '../img/img-project/beecloon.png';
 import opus from '../img/img-project/opus.png';
+import maison_ayele from '../img/img-project/maison_ayele.png';
+import plateformeacc from '../img/img-project/plateformeacc.png';
 import '../styles/Projects.css';
 
 const Projects = () => {
-  const initialProjectsToShow = 3; // Nombre initial de projets à afficher
+  const initialProjectsToShow = 3;
   const [projectsToShow, setProjectsToShow] = useState(initialProjectsToShow);
 
   const projects = [
     {
       id: 1,
       title: 'Portfolio Website',
-      description: 'My personal portfolio website built with React.',
-      imageUrl: opus,
+      description: 'My personal portfolio website built with React. CSS effects, animations, responsive.',
+      imageUrl: portfolio,
       projectLink: 'lien_vers_le_projet',
       codeLink: 'lien_vers_le_code_source'
     },
     {
       id: 2,
-      title: 'E-commerce App',
-      description: 'An e-commerce web application built with Laravel and Vue.js.',
-      imageUrl: opus,
+      title: 'Bee Cloon - E-commerce',
+      description: 'An e-commerce web application built with PHP and Wordpres. Pack building feature, click and collect.',
+      imageUrl: beecloon,
       projectLink: 'lien_vers_le_projet',
       codeLink: 'lien_vers_le_code_source'
     },
-    // Ajoute d'autres projets ici
     {
       id: 3,
-      title: 'Opus Musiques',
-      description: 'Media and blog website built with Laravel and Angular',
+      title: 'Opus Musiques - Media',
+      description: 'Web media for music lovers. Built with Laravel and Tailwind. Backend management, user authentication.',
       imageUrl: opus,
       projectLink: 'lien_vers_le_projet',
       codeLink: 'lien_vers_le_code_source'
     },
     {
       id: 4,
-      title: 'Project 4',
-      description: 'Description 4',
-      imageUrl: opus,
+      title: 'Maison Ayele',
+      description: 'Fixing out of stock issues on the website and adding new features. Built with PHP and Wordpress.',
+      imageUrl: maison_ayele,
       projectLink: 'lien_vers_le_projet',
       codeLink: 'lien_vers_le_code_source'
     },
     {
       id: 5,
-      title: 'Project 5',
-      description: 'Description 5',
-      imageUrl: opus,
+      title: 'Plateforme Accompagnement',
+      description: 'A diary of artists and events that accompany and support Réunionese authors. Built with PHP and Wordpress.',
+      imageUrl: plateformeacc,
       rojectLink: 'lien_vers_le_projet',
-      codeLink: 'lien_vers_le_code_source'
-    },
-    {
-      id: 6,
-      title: 'Project 6',
-      description: 'Description 6',
-      imageUrl: opus,
-      projectLink: 'lien_vers_le_projet',
-      codeLink: 'lien_vers_le_code_source'
-    },
-    {
-      id: 7,
-      title: 'Project 7',
-      description: 'opqkidfshgiqehv giqejng iovqunghiopqv erhj,giop q,erghiopqurghnqeirg herio,guhvqneiorghureniovgherfdgdfgfdg dfgdfg sdf gdfgdf gdfgdf g f gdfg g dfgdf gdfiuwvxgh er',
-      imageUrl: opus,
-      projectLink: 'lien_vers_le_projet',
-      codeLink: 'lien_vers_le_code_source'
-    },
-    {
-      id: 8,
-      title: 'Project 8',
-      description: 'Description 8',
-      imageUrl: opus,
-      projectLink: 'lien_vers_le_projet',
-      codeLink: 'lien_vers_le_code_source'
-    },
-    {
-      id: 9,
-      title: 'Project 9',
-      description: 'Description 9',
-      imageUrl: opus,
-      projectLink: 'lien_vers_le_projet',
-      codeLink: 'lien_vers_le_code_source'
-    },
-    {
-      id: 10,
-      title: 'Project 10',
-      description: 'Description 10',
-      imageUrl: opus,
-      projectLink: 'lien_vers_le_projet',
       codeLink: 'lien_vers_le_code_source'
     }
   ];
@@ -100,7 +63,8 @@ const Projects = () => {
 
   return (
     <section className="projects">
-      <h2>WORK</h2>
+      <h2>Work</h2>
+      <p className='section-infos'>Here are some of the personal and corporate projects I've enjoyed doing.</p>
       <div className="project-cards">
         {projects.slice(0, projectsToShow).map(project => (
           <ProjectCard key={project.id} project={project} />
